01efee922efe5548869b701a144dcd85a569cab9
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2022-05-17 18:55:09 +0000

    --wip-- [skip ci]

--- tennyson/cli.py
+++ tennyson/cli.py
@@ -13,3 +13,20 @@ def main():
 def password():
     for p in tennyson.password.for_display_passwords():
         print(p)
+
+@main.command()
+def math_quiz():
+    import random
+    import math
+    while True:
+        a = math.ceil((10 * random.random())**2)
+        b = math.ceil((10 * random.random())**2)
+        op = random.choice(['*', '/'])
+        if op == '*':
+            c = a * b
+        if op == '/':
+            c = a / b
+        print(f'{a} {op} {b}')
+        input()
+        print(f'{c}')
+        input()
